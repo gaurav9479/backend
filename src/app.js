ce752930122b47b8fc154d9dcd18a6d9f96e7f0e
gaurav <gauravshivmurat2@gmail.com>
2025-06-17 14:57:14 +0530

    cloudinary built

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express"
 import cookieParser from "cookie-parser"
 import cors from "cors"
 const app=express()
-app.use(cors({
+app.use(cors({ 
     origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
@@ -10,3 +10,12 @@ app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+//routes decleration
+app.use("/api/v1/users",userRouter);
+
+//http://localhost:8000/api/v1/users/register
+
+export {app}
